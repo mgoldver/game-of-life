eaa8b8e21faeac5a9d1309e644089285af43a011
Michael Goldverg <michael.goldverg@gmail.com>
2015-01-16 21:24:21 -0500

    changes stars to pluses

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,31 +1,31 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    LIVE_CELL("*"), DEAD_CELL(".");
+	LIVE_CELL("+"), DEAD_CELL(".");
 
-    private String symbol;
+	private String symbol;
 
-    private Cell(final String initialSymbol) {
-        this.symbol = initialSymbol;
-    }
+	private Cell(final String initialSymbol) {
+		this.symbol = initialSymbol;
+	}
 
-    @Override
-    public String toString() {
-        return symbol;
-    }
+	@Override
+	public String toString() {
+		return symbol;
+	}
 
-    static Cell fromSymbol(final String symbol) {
-        Cell cellRepresentedBySymbol = null;
-        for (Cell cell : Cell.values()) {
-            if (cell.symbol.equals(symbol)) {
-                cellRepresentedBySymbol = cell;
-                break;
-            }
-        }
-        return cellRepresentedBySymbol;
-    }
+	static Cell fromSymbol(final String symbol) {
+		Cell cellRepresentedBySymbol = null;
+		for (Cell cell : Cell.values()) {
+			if (cell.symbol.equals(symbol)) {
+				cellRepresentedBySymbol = cell;
+				break;
+			}
+		}
+		return cellRepresentedBySymbol;
+	}
 
-    public String getSymbol() {
-        return symbol;
-    }
+	public String getSymbol() {
+		return symbol;
+	}
 }
